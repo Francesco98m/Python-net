84c75b9bc05c4080d419b05181ae4f0dcd7b3fa9
lorenzosquadrani <55916809+lorenzosquadrani@users.noreply.github.com>
2020-02-03 22:29:27 +0100

    Update losses.py

--- color_classifier/losses.py
+++ color_classifier/losses.py
@@ -2,15 +2,19 @@ import torch
 
 def SVM_loss(scores, y, delta = 1.0):
     """
-    scores is a torch.tensor object of shape (n_categories, n_examples); every column of scores shall the score of one example.
+    scores is a torch.tensor object of shape (n_categories, n_examples); 
+    every column shall contain the score of one example.
     y is a torch.tensor object of shape (n_examples,); it shall contain the correct categories of every example.
     delta is a float number; it's a hyperparameter of this loss function.
     """
     n_examples = scores.shape[1]  
     errors = scores - scores[y,list(range(n_examples))] 
 # Happens the following: 
-#scores is a tensor of shape (n_categories, n_examples) and the second term is a tensor of shape (n_examples,); subtracting the latter to the first you will subtract to all the elements of the first column of scores the first element of the second term, and so on. 
-#So from scores we must choose for every column (every score) the element which represent the correct categorie (for every column the position of the corrent element is contained in y). In order to do that we write:
+#scores is a tensor of shape (n_categories, n_examples) and the second term is a tensor of shape (n_examples,); 
+#subtracting the latter to the first you will subtract to all the elements of the first column of scores the first element 
+#of the second term, and so on. 
+#So from scores we must choose for every column (every score) the element which represent the correct categorie (for every 
+#column the position of the corrent element is contained in y). In order to do that we write:
 # scores[y,list(range(n_examples))] 
 # which is equal to:
 # scores[[correct_position_1, correct_position_2, ..., correct_position_nexamples],[0,1,2,...,n_examples]] 
@@ -27,7 +31,7 @@ def SVM_loss(scores, y, delta = 1.0):
 def cross_entropy (scores, y):
     """
     scores is a torch.tensor object of shape (n_categories, n_examples);
-    every column of scores contains the score of one example.
+    every column contains the score of one example.
     y is a torch.tensor object of shape (n_examples,); it shall contain the correct categories of every example.
     The function return the cross entropy loss (already normalized)
     """
